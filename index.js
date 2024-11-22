343aefa840267e7706ea8a21de905e78b6acec89
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2024-11-22 17:31:33 +0100

    phonebook backend step 6 implemented

--- index.js
+++ index.js
@@ -1,90 +1,104 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-
 
 app.use(express.json());
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+  {
+    id: "9",
+    name: "eneye",
+    number: "99999999",
+  },
+];
 
-app.get('/', (request, response)=>{
-    response.end('<h1>Persons API</h1>')
-})
+app.get("/", (request, response) => {
+  response.end("<h1>Persons API</h1>");
+});
 
-app.get('/api/persons', (request, response)=>{
-    response.json(persons);
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response)=> {
-    const id = request.params.id;
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
 
-    const person = persons.find(p=>p.id === id)
+  const person = persons.find((p) => p.id === id);
 
-    if(person){
-        response.json(person);
-    }else{
-        response.status(404).end();
-    }
-})
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
 
-app.delete('/api/persons/:id', (request, response)=>{
-    const id = request.params.id;
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
 
-    persons = persons.filter(p=>p.id !== id);
+  persons = persons.filter((p) => p.id !== id);
 
-    response.status(204).end()
-})
+  response.status(204).end();
+});
 const generateId = () => {
-    return Math.floor(Math.random() * 1000000);
-}
-app.post('/api/persons', (request, response)=>{
-    const body = request.body;
+  return Math.floor(Math.random() * 1000000);
+};
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
-    if(!body.number || !body.name){
-        return response.status(400).end()
-    }
-const id = generateId();
-console.log('id', id)
-     const newPerson = {
-        id: id,
-        name: body.name,
-        number: body.number
-    }
-    persons.concat(newPerson)
+  if (!body.number || !body.name) {
+    return response.status(400).json({ error: "missing values (name|number)" });
+  }
 
-    response.json(newPerson)
-})
+  const personFound = persons.find((p) => {
+    
+    console.log('p',p.name)
+    console.log('b', body.name)
+    return p.name === body.name});
+console.log('pf',personFound)
+  if (personFound) {
+    return response.status(400).json({ error: "name must be unique" });
+  }
 
-app.get('/info', (request, response)=>{
-    let message = `<p>Phonebook has info for ${persons.length} people</p>`
-    message += `<p>${new Date()}</p>`
+  const id = generateId();
+  console.log("id", id);
+  const newPerson = {
+    id: id,
+    name: body.name,
+    number: body.number,
+  };
+  persons.concat(newPerson);
 
-    response.send(message);
-})
+  response.json(newPerson);
+});
 
+app.get("/info", (request, response) => {
+  let message = `<p>Phonebook has info for ${persons.length} people</p>`;
+  message += `<p>${new Date()}</p>`;
 
-const PORT = 3001
-app.listen(PORT, ()=>{
-    console.log(`listening on port ${PORT}`)
-})
+  response.send(message);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
