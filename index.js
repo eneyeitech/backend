b89577659dda208c8ec2b26feb0b50a2b6f4727e
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2024-11-29 10:26:39 +0100

    Phonebook step 9 - 11 implemented

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
 
 const app = express();
+
+app.use(express.static('dist'))
+
+app.use(cors())
 
 app.use(express.json());
 
@@ -109,7 +114,7 @@ app.get("/info", (request, response) => {
   response.send(message);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # For Helsinki Fullstackopen course
+
+# Project cloud url
+
+[https://backend-restless-fire-3120.fly.dev/]
